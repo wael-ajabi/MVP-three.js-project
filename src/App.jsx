837fe07a13b06e86a63239eebc6cbd9741d77c33
tababighassane <tabbabighassen@gmail.com>
2022-03-19 19:27:09 +0100

    improve performence

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 const Vis = () => {
-  const { useRef, useEffect, useState } = React;
+  const { useRef, useLayoutEffect, useState, useEffect } = React;
   const mount = useRef(null);
   const [isAnimating, setAnimating] = useState(true);
   const [legColor, setlegColor] = useState(null);
@@ -13,111 +13,111 @@ const Vis = () => {
 
   const controlsAnimation = useRef(null);
 
-  useEffect(() => {
-    let width = mount.current.clientWidth;
-    let height = mount.current.clientHeight;
-    let frameId;
+  let width = screen.width;
+  let height = screen.height;
+  let frameId;
 
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setClearColor("#262837");
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+  const renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setClearColor("#262837");
+  camera.position.z = 4;
+  renderer.setSize(width, height);
+  const renderScene = () => {
+    renderer.render(scene, camera);
+  };
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  const gltfloader = new GLTFLoader();
+  //Light
+  const pointLight = new THREE.AmbientLight(0xffffff, 3);
+  scene.add(pointLight);
 
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff });
-    const cube = new THREE.Mesh(geometry, material);
+  function createsplint(color) {
+    let pos_x = 0,
+      pos_y = 0,
+      pos_z = 0,
+      scale_x = 0.4,
+      scale_y = 0.4,
+      scale_z = 0.4;
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath("/js/libs/draco/"),
+      gltfloader.setDRACOLoader(dracoLoader),
+      gltfloader.load("./src/last.glb", function (gltf) {
+        if (color) {
+          gltf.scene.children[0].material.color.set(color);
+        }
+        (gltf.scene.rotation.y = 3.1),
+          gltf.scene.position.set(pos_x, pos_y, pos_z),
+          gltf.scene.scale.set(scale_x, scale_y, scale_z),
+          (gltf.scene.castShadow = !0),
+          (gltf.scene.receiveShadow = !0),
+          scene.add(gltf.scene),
+          (gltf.scene.userData.ground = !0);
+      });
+  }
 
-    camera.position.z = 4;
-    // scene.add(cube);
-    renderer.setClearColor("0x2929");
+  function createleg(color) {
+    let pos_x = 0,
+      pos_y = 0,
+      pos_z = 0,
+      scale_x = 0.38,
+      scale_y = 0.38,
+      scale_z = 0.38;
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath("/js/libs/draco/"),
+      gltfloader.setDRACOLoader(dracoLoader),
+      gltfloader.load("./src/leg.gltf", function (gltf) {
+        if (color) {
+          gltf.scene.children[0].material.color.set(color);
+        }
+        (gltf.scene.rotation.y = 3.1),
+          gltf.scene.position.set(pos_x, pos_y, pos_z),
+          gltf.scene.scale.set(scale_x, scale_y, scale_z),
+          (gltf.scene.castShadow = !0),
+          (gltf.scene.receiveShadow = !0),
+          scene.add(gltf.scene),
+          (gltf.scene.userData.ground = !0);
+      });
+  }
+
+  const handleResize = () => {
+    width = mount.current.clientWidth;
+    height = mount.current.clientHeight;
     renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderScene();
+  };
 
-    const renderScene = () => {
-      renderer.render(scene, camera);
-    };
+  const animate = () => {
+    renderScene();
+    frameId = window.requestAnimationFrame(animate);
+  };
 
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    const gltfloader = new GLTFLoader();
-    //Light
-    const pointLight = new THREE.AmbientLight(0xffffff, 3);
-    scene.add(pointLight);
+  const start = () => {
+    if (!frameId) {
+      frameId = requestAnimationFrame(animate);
+    }
+  };
+
+  useLayoutEffect(() => {
     //add splint
-    function createsplint() {
-      let pos_x = 0,
-        pos_y = 0,
-        pos_z = 0,
-        scale_x = 0.4,
-        scale_y = 0.4,
-        scale_z = 0.4;
-      const dracoLoader = new DRACOLoader();
-      dracoLoader.setDecoderPath("/js/libs/draco/"),
-        gltfloader.setDRACOLoader(dracoLoader),
-        gltfloader.load("./src/last.glb", function (gltf) {
-			if(splintColor){gltf.scene.children[0].material.color.set(splintColor)}
-          (gltf.scene.rotation.y = 3.1),
-            gltf.scene.position.set(pos_x, pos_y, pos_z),
-            gltf.scene.scale.set(scale_x, scale_y, scale_z),
-            (gltf.scene.castShadow = !0),
-            (gltf.scene.receiveShadow = !0),
-            scene.add(gltf.scene),
-            (gltf.scene.userData.ground = !0);
-        });
-    }
     createsplint();
-    //create leg
-    function createleg() {
-      let pos_x = 0,
-        pos_y = 0,
-        pos_z = 0,
-        scale_x = 0.38,
-        scale_y = 0.38,
-        scale_z = 0.38;
-      const dracoLoader = new DRACOLoader();
-      dracoLoader.setDecoderPath("/js/libs/draco/"),
-        gltfloader.setDRACOLoader(dracoLoader),
-        gltfloader.load("./src/leg.gltf", function (gltf) {
-			if(legColor){gltf.scene.children[0].material.color.set(legColor)}
-          (gltf.scene.rotation.y = 3.1),
-            gltf.scene.position.set(pos_x, pos_y, pos_z),
-            gltf.scene.scale.set(scale_x, scale_y, scale_z),
-            (gltf.scene.castShadow = !0),
-            (gltf.scene.receiveShadow = !0),
-            scene.add(gltf.scene),
-            (gltf.scene.userData.ground = !0);
-        });
-    }
-    createleg();
-    const handleResize = () => {
-      width = mount.current.clientWidth;
-      height = mount.current.clientHeight;
-      renderer.setSize(width, height);
-      camera.aspect = width / height;
-      camera.updateProjectionMatrix();
-      renderScene();
-    };
-	const animate = () => {
-		renderScene();
-		frameId = window.requestAnimationFrame(animate);
-	  };
-  
-	  const start = () => {
-		if (!frameId) {
-		  frameId = requestAnimationFrame(animate);
-		}
-	  };
-	start();
 
+    //create leg
+    createleg();
+
+    start();
     mount.current.appendChild(renderer.domElement);
     window.addEventListener("resize", handleResize);
-	controlsAnimation.current = { start };
-
-
+    // controlsAnimation.current = { start };
     return () => {
       window.removeEventListener("resize", handleResize);
       mount.current.removeChild(renderer.domElement);
     };
-  }, [legColor,splintColor]);
+  });
+
   return (
     <div>
       <div
@@ -126,12 +126,26 @@ const Vis = () => {
         onClick={() => setAnimating(!isAnimating)}
       />
       <div id="options">
-        <label id="click">
-          leg:
-        </label>
-        <input id="myColor" type="color" defaultValue="#f6b73c" className="colors" onChange={(e)=>{setlegColor(e.target.value)}} />
+        <label id="click">leg:</label>
+        <input
+          id="myColor"
+          type="color"
+          defaultValue="#f6b73c"
+          className="colors"
+          onChange={(e) => {
+            createleg(e.target.value);
+          }}
+        />
         <label id="click1">splint:</label>
-        <input id="myColor1" type="color" className="colors" defaultValue="#ffffff"  onChange={(e)=>{setsplintColor(e.target.value)}} />
+        <input
+          id="myColor1"
+          type="color"
+          className="colors"
+          defaultValue="#ffffff"
+          onChange={(e) => {
+            createsplint(e.target.value);
+          }}
+        />
       </div>
     </div>
   );
